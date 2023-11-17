69ee1f7bdf33ecbd321cb775b82cc7dcf89f701d
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-17 23:09:20 +0100

    Refactor TaskList component

--- src/components/ui/TaskList.tsx
+++ src/components/ui/TaskList.tsx
@@ -1,15 +1,10 @@
-import { useRecoilState, useRecoilValue } from "recoil"
-import { activeCategoryTasksState, activeTaskCategorySelector, taskCategoriesListSelector, taskObjectState } from "../../config/states"
-import { DeleteIcon } from "@chakra-ui/icons"
-import { Box, Checkbox } from "@chakra-ui/react"
-import { task, taskCategory } from "../../types/taskapi"
+import { useRecoilValue } from "recoil"
+import { activeCategoryTasksSelector } from "../../config/states"
+import { task } from "../../types/taskapi"
 import TaskItem from "./TaskItem"
 
 export default function TaskList() {
-    const Taskobject = useRecoilValue(taskObjectState) 
-    const taskCategoryList = useRecoilValue<taskCategory[]>(taskCategoriesListSelector)
-    const activeTaskCategory = useRecoilValue<number>(activeTaskCategorySelector)
-    const [activeCategoryTasks, setActiveCategoryTasks] = useRecoilState<task[]>(activeCategoryTasksState)
+    const activeCategoryTasks = useRecoilValue<task[]>(activeCategoryTasksSelector)
 
 
     return (
