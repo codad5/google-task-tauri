1fbff01ed1c8ee9a842c4d283c6969160826d2b9
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-17 21:40:51 +0100

    Add delete button to TaskItem component

--- src/components/ui/TaskItem.tsx
+++ src/components/ui/TaskItem.tsx
@@ -1,12 +1,26 @@
 import { DeleteIcon } from "@chakra-ui/icons";
-import { Box, Checkbox } from "@chakra-ui/react";
+import { Box, Checkbox, IconButton } from "@chakra-ui/react";
 import { task } from "../../types/taskapi";
+import { useState } from "react";
 
 
 export default function TaskItem({ task, handleTaskCheck, key }: { task: task, handleTaskCheck: (task: task) => void, key: number }) {
+    const [isHovered, setIsHovered] = useState(false);
     return (
-        <Box p="2" key={key}>
-            <Checkbox isChecked={task.completed} onChange={() => handleTaskCheck(task)}>{task.name} <DeleteIcon /></Checkbox>
+        <Box p="2" key={key} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <Checkbox isChecked={task.completed} onChange={() => handleTaskCheck(task)}>
+                {task.name}
+                {isHovered && (
+                    <IconButton
+                        ml="2"
+                        aria-label="Delete task"
+                        icon={<DeleteIcon />}
+                        onClick={() => {
+                        // Handle delete logic here
+                        }}
+                    />
+                )}
+            </Checkbox>
         </Box>
     )
 }
