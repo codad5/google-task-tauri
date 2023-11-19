ae76376e9013b2f30e635701287c1361b13cdd4f
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-19 17:12:35 +0100

    Fix environment variable handling in main.yml and credentials.ts

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,2 +1,2 @@
-export const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string
-export const CLIENT_SECRET = import.meta.env.VITE_GOOGLE_CLIENT_SECRET as string
+export const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? process.env.VITE_GOOGLE_CLIENT_ID as string
+export const CLIENT_SECRET = import.meta.env.VITE_GOOGLE_CLIENT_SECRET ?? process.env.VITE_GOOGLE_CLIENT_SECRET as string
