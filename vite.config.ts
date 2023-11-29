38e230ddc53701ae511e9d04bd1ffe980835990a
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-29 14:59:11 +0100

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig(async () => ({
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
-  base: process.env.NODE_ENV === "production" ? "/google-task-tauri/" : "/",
+  base: "/google-task-tauri/"
 }));
