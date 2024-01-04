490836c4348d2318d0fc9adeabc47a1bacc992e1
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-01-04 03:27:08 +0100

    Update page.tsx and layout.tsx for seo

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Header from '@/components/ui/header'
 import Hero from '@/components/ui/hero'
-import { getPostContent, getPostsAndMetadata } from '@/libs/posts';
+import { getPostContent, getPostMetadata, getPostsAndMetadata } from '@/libs/posts';
 import { postsType } from '@/libs/types';
 import matter from 'gray-matter';
 import { notFound } from 'next/navigation';
 import Markdown from 'markdown-to-jsx';
+import { Metadata } from 'next';
 
 
 export const generateStaticParams = () => {
@@ -14,6 +15,41 @@ export const generateStaticParams = () => {
     return posts.map((post) => ({
             slug: post.slug 
     }))
+}
+
+export async function generateMetadata({ params, searchParams }: { params: { slug: string }, searchParams: any }): Promise<Metadata> {
+    const { slug } = params;
+    const postContents = getPostMetadata(slug, 'static-pages')
+    if(!postContents) return {};
+    const { title, date, description, tags, image } = postContents
+    return {
+      title,
+      description : `Google Task Desktop Client - ${description}`,
+      keywords: tags ?? ['google', 'task', 'desktop', 'client', 'electron', 'react', 'typescript', 'nextjs', 'codad5'],
+      authors: [{ name: 'Chibueze Michael Aniezeofor', url: 'https://codad5.me' }],
+      creator: 'Chibueze Michael Aniezeofor',
+      publisher: 'Chibueze Michael Aniezeofor',
+      alternates: {},
+      formatDetection: {
+          email: true,
+          address: true,
+          telephone: true,
+      },
+      openGraph: {
+          title,
+          description,
+          url: `https://google-task.codad5.me//${slug}`,
+          type: 'website',
+          images: [
+              {
+                  url: image ?? 'https://google-task.codad5.me/google-task.png',
+                  width: 800,
+                  height: 600,
+                  alt: title,
+              },
+          ],
+      },
+    }
 }
 
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,30 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Google Tasks',
-  description: 'Google Tasks Desktop App for Windows, Mac & Linux',
+  description: 'Google Tasks Desktop App for Windows, Mac & Linux built using React & Rust (Tauri)',
+  keywords: ['google', 'task', 'desktop', 'client', 'electron', 'react', 'typescript', 'nextjs', 'codad5', 'windows', 'mac', 'linux', 'tauri', 'lightweight', 'fast', 'cross-platform'],
+  authors: [{ name: 'Chibueze Michael Aniezeofor', url: 'https://codad5.me' }],
+  creator: 'Chibueze Michael Aniezeofor',
+  publisher: 'Chibueze Michael Aniezeofor',
+  alternates: {},
+  formatDetection: {
+      email: true,
+      address: true,
+      telephone: true,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'standard',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({
